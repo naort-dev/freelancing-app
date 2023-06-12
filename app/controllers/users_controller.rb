ec6997b3a6686334892a16a64aeefd1d79343691
naort-dev <naor.tzairi.upwork@gmail.com>
2023-06-12 17:04:42 +0530

    Basic email structure ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      UserMailer.account_activation(@user).deliver_later
+      # session[:user_id] = @user.id
       redirect_to root_path, notice: 'Thank you for signing up!'
     else
       flash.now[:error] = 'Please try again'
