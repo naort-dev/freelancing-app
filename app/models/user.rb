844d41f244db4f65e7bbbda2bb6742fbdf2aea89
naort-dev <naor.tzairi.upwork@gmail.com>
2023-06-19 23:11:37 +0530

    Added nested attribute to categories in User mappings

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
       indexes :id, type: :integer
       indexes :email
       indexes :role, type: :keyword
-      indexes :categories do
+      indexes :categories, type: :nested do
         indexes :name, type: :text
       end
     end
