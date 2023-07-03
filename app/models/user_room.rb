e17634b9d4d4a2600ac85f4686a7a012db46674f
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-03 20:48:56 +0530

    The design part of the chatboard is done

--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -2,4 +2,8 @@ class UserRoom < ApplicationRecord
   belongs_to :user1, class_name: 'User'
   belongs_to :user2, class_name: 'User'
   belongs_to :room
+
+  def other_user(user)
+    user == user1 ? user2 : user1
+  end
 end
