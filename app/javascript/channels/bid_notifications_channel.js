2bfa39a618da96ab26a0a89b9476a44d61933d0e
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-04 21:28:33 +0530

    Mark as read functionality working now

--- app/javascript/channels/bid_notifications_channel.js
+++ app/javascript/channels/bid_notifications_channel.js
@@ -20,7 +20,10 @@ document.addEventListener("turbolinks:load", () => {
         notificationItem.href = "/projects/" + data.project_id;
         notificationItem.textContent = `Your bid to ${data.bid_project_title} is ${data.bid_status}`;
 
-        notificationItem.addEventListener("click", function () {
+        // ...
+        notificationItem.addEventListener("click", function (event) {
+          event.preventDefault();
+
           fetch(`/notifications/${data.notification_id}/mark_as_read`, {
             method: "POST",
             headers: {
@@ -28,8 +31,27 @@ document.addEventListener("turbolinks:load", () => {
               "Content-Type": "application/json",
               Accept: "application/json"
             }
-          });
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              if (data.success) {
+                // Add a 'read' class to the notification item
+                notificationItem.classList.add("text-muted");
+
+                // Then navigate to the project page
+                window.location.href = notificationItem.href;
+              }
+            })
+            .catch((error) => {
+              console.error("There has been a problem with your fetch operation:", error);
+            });
         });
+        // ...
 
         const notificationList = document.getElementById("notificationList");
         notificationList.insertBefore(notificationItem, notificationList.firstChild);
--- app/javascript/packs/notifications.js
+++ app/javascript/packs/notifications.js
@@ -18,13 +18,16 @@ function loadNotifications() {
       notifications.forEach((notification) => {
         const notificationItem = document.createElement("a");
         notificationItem.classList.add("dropdown-item", "text-wrap");
+        if(notification.read) {
+          notificationItem.classList.add("text-muted");
+        }
         notificationItem.href = "/projects/" + notification.project_id;
         notificationItem.textContent = notification.message;
 
-        // Add click event listener
         // ...
-        // ...
-        notificationItem.addEventListener("click", function () {
+        notificationItem.addEventListener("click", function (event) {
+          event.preventDefault();
+
           fetch(`/notifications/${notification.id}/mark_as_read`, {
             method: "POST",
             headers: {
@@ -32,10 +35,26 @@ function loadNotifications() {
               "Content-Type": "application/json",
               Accept: "application/json"
             }
-          });
-        });
-        // ...
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              if (data.success) {
+                // Add a 'read' class to the notification item
+                notificationItem.classList.add("text-muted");
 
+                // Then navigate to the project page
+                window.location.href = notificationItem.href;
+              }
+            })
+            .catch((error) => {
+              console.error("There has been a problem with your fetch operation:", error);
+            });
+        });
         // ...
 
         notificationList.appendChild(notificationItem);
