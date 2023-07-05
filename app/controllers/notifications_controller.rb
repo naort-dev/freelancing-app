86466e408eb37fd6c0ec64b8729632f6debee0b2
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-05 13:07:02 +0530

    Refactored the notifications.js file and the notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,31 +1,38 @@
 class NotificationsController < ApplicationController
   def count
-    full_count = current_user.notifications.count
-    count = current_user.notifications.where(read: false).count
-    render json: { count:, full_count: }
+    notifications = current_user.notifications
+    full_count = notifications.count
+    unread_count = notifications.where(read: false).count
+    render json: { count: unread_count, full_count: }
   end
 
   def fetch_notifications
     limit = params[:limit] || current_user.notifications.count
-    notifications = current_user.notifications.order('created_at DESC').limit(limit)
+    notifications = current_user.notifications.includes(:actor, :project, :bid).order(created_at: :desc).limit(limit)
     render json: notifications.to_json(include: { actor: { only: :email }, project: { only: :title },
                                                   bid: { only: :bid_status } })
   end
 
   def mark_as_read
     notification = current_user.notifications.find(params[:id])
-    notification.update!(read: true)
+    notification.update(read: true)
     render json: { success: true }
   end
 
   def mark_all_as_read
     notifications = current_user.notifications.where(read: false)
-    notifications.update_all(read: true)
-    render json: { success: true }
+    success = true
+    notifications.each do |notification|
+      unless notification.update(read: true)
+        success = false
+        break
+      end
+    end
+    render json: { success: }
   end
 
   def delete_read
-    Notification.where(read: true).destroy_all
+    current_user.notifications.where(read: true).destroy_all
     render json: { success: true }
   end
 end
--- app/javascript/packs/notifications.js
+++ app/javascript/packs/notifications.js
@@ -8,37 +8,15 @@ function fetchNotificationsCount() {
     .then((response) => response.json())
     .then((data) => {
       notificationBadge.innerText = data.count;
-      console.log(data.count);
-      if (data.full_count > 5) {
-        showAllButton.style.display = "block";
-        console.log("block display");
-        console.log(showAllButton);
-      } else {
-        showAllButton.style.display = "none";
-      }
-
-      if (data.full_count > 0) {
-        markAllAsReadButton.style.display = "block";
-      } else {
-        markAllAsReadButton.style.display = "none";
-      }
-
-      if((data.full_count - data.count) > 0) {
-        deleteReadNotificationsButton.style.display = "block";
-      } else {
-        deleteReadNotificationsButton.style.display = "none";
-      }
+      showAllButton.style.display = data.full_count > 5 ? "block" : "none";
+      markAllAsReadButton.style.display = data.full_count > 0 ? "block" : "none";
+      deleteReadNotificationsButton.style.display = data.full_count - data.count > 0 ? "block" : "none";
     });
 }
 
 function loadNotifications(showAll = false) {
   const notificationList = document.getElementById("notificationList");
   let notificationContainer = document.getElementById("notificationContainer");
-
-  if (showAll) {
-    console.log(notificationList);
-    console.log(notificationContainer);
-  }
 
   if (!notificationContainer) {
     notificationContainer = document.createElement("div");
@@ -47,18 +25,11 @@ function loadNotifications(showAll = false) {
   }
 
   notificationContainer.innerHTML = "";
-  console.log(`/notifications/fetch_notifications${showAll ? "" : "?limit=5"}`);
   fetch(`/notifications/fetch_notifications${showAll ? "" : "?limit=5"}`)
     .then((response) => response.json())
     .then((notifications) => {
       notifications.forEach((notification) => {
-        const notificationItem = document.createElement("a");
-        notificationItem.classList.add("dropdown-item", "text-wrap");
-        if (notification.read) {
-          notificationItem.classList.add("text-muted");
-        }
-        notificationItem.href = "/projects/" + notification.project_id;
-        notificationItem.textContent = notification.message;
+        const notificationItem = createNotificationItem(notification);
 
         notificationItem.addEventListener("click", function (event) {
           event.preventDefault();
@@ -97,45 +68,13 @@ function fetchNotifications() {
   const currentUserId = document.querySelector("body").dataset.currentUserId;
 
   if (currentUserId != "") {
-    const notificationList = document.getElementById("notificationList");
-
-    const showAllButton = document.createElement("button");
-    showAllButton.id = "showAllButton";
-    showAllButton.style.display = "none";
-    showAllButton.classList.add("btn", "btn-primary", "btn-sm", "mx-1", "my-1");
-    showAllButton.textContent = "Show All";
-    showAllButton.addEventListener("click", function (event) {
-      console.log("Clicked!");
-      event.stopPropagation();
-
+    addClickListener(document.getElementById("showAllButton"), function () {
       loadNotifications(true);
-
       showAllButton.style.display = "none";
     });
-    notificationList.appendChild(showAllButton);
 
-    const markAllAsReadButton = document.createElement("button");
-    markAllAsReadButton.id = "markAllAsReadButton";
-    showAllButton.style.display = "none";
-    markAllAsReadButton.classList.add("btn", "btn-warning", "btn-sm", "mx-1", "my-1");
-    markAllAsReadButton.textContent = "Mark All as Read";
-    markAllAsReadButton.addEventListener("click", function (event) {
-      event.stopPropagation();
-
-      fetch(`/notifications/mark_all_as_read`, {
-        method: "POST",
-        headers: {
-          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
+    addClickListener(document.getElementById("markAllAsReadButton"), function () {
+      fetchWithCsrfToken("/notifications/mark_all_as_read", "POST")
         .then((data) => {
           if (data.success) {
             const notificationItems = document.querySelectorAll(".dropdown-item");
@@ -148,30 +87,9 @@ function fetchNotifications() {
           console.error("There has been a problem with your fetch operation:", error);
         });
     });
-    notificationList.appendChild(markAllAsReadButton);
 
-    const deleteReadNotificationsButton = document.createElement("button");
-    deleteReadNotificationsButton.id = "deleteReadNotificationsButton";
-    deleteReadNotificationsButton.style.display = "none";
-    deleteReadNotificationsButton.classList.add("btn", "btn-danger", "btn-sm", "mx-1", "my-1");
-    deleteReadNotificationsButton.textContent = "Delete Read Notifications";
-    deleteReadNotificationsButton.addEventListener("click", function (event) {
-      event.stopPropagation();
-
-      fetch(`/notifications/delete_read`, {
-        method: "POST",
-        headers: {
-          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
+    addClickListener(document.getElementById("deleteReadNotificationsButton"), function () {
+      fetchWithCsrfToken("/notifications/delete_read", "POST")
         .then((data) => {
           if (data.success) {
             const notificationItems = document.querySelectorAll(".dropdown-item.text-muted");
@@ -184,7 +102,6 @@ function fetchNotifications() {
           console.error("There has been a problem with your fetch operation:", error);
         });
     });
-    notificationList.appendChild(deleteReadNotificationsButton);
 
     fetchNotificationsCount();
     loadNotifications();
@@ -192,3 +109,45 @@ function fetchNotifications() {
 }
 
 document.addEventListener("turbolinks:load", fetchNotifications);
+
+function createButton(id, textContent, btnClass) {
+  const button = document.createElement("button");
+  button.id = id;
+  button.style.display = "none";
+  button.classList.add("btn", btnClass, "btn-sm", "mx-1", "my-1");
+  button.textContent = textContent;
+  return button;
+}
+
+async function fetchWithCsrfToken(url, method) {
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+}
+
+function addClickListener(button, callback) {
+  button.addEventListener("click", function (event) {
+    event.stopPropagation();
+    callback();
+  });
+}
+
+function createNotificationItem(notification) {
+  const notificationItem = document.createElement("a");
+  notificationItem.classList.add("dropdown-item", "text-wrap");
+  if (notification.read) {
+    notificationItem.classList.add("text-muted");
+  }
+  notificationItem.href = "/projects/" + notification.project_id;
+  notificationItem.textContent = notification.message;
+  return notificationItem;
+}
