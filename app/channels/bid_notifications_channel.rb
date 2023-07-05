7d02caf06373245bdcd62c7bb98de0e793548be5
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-05 23:02:04 +0530

    Fixed the rubocop offenses for channels

--- app/channels/bid_notifications_channel.rb
+++ app/channels/bid_notifications_channel.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BidNotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "bid_notifications_channel_#{params[:user_id]}"
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from 'room_channel'
     room = Room.find(params[:room_id])
     stream_for room
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
+  def unsubscribed; end
 end
