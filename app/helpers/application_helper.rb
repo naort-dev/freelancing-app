5e288ec226057eec61cf72e17cf7089505db6358
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-05 23:07:55 +0530

    Fixed the rest of the rubocop offenses in the app directory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def flash_class(level)
     case level
     when 'error'
       'alert-danger'
-    when 'notice'
-      'alert-info'
     when 'success'
       'alert-success'
     else
