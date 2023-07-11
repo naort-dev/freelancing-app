f22b7a1e6630edd2a4ed732bd4cc11e7cfbd3003
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-11 16:12:18 +0530

    Fixed the rubocop offenses for config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ElasticCategories
-  ELASTICSEARCH_CONFIG_FILE = 'config/elasticsearch.yml'.freeze
+  ELASTICSEARCH_CONFIG_FILE = 'config/elasticsearch.yml'
   Rails.configuration.elasticsearch = YAML.unsafe_load_file(ELASTICSEARCH_CONFIG_FILE)[Rails.env].deep_symbolize_keys
 
   Elasticsearch::Model.client = Elasticsearch::Client.new Rails.configuration.elasticsearch
