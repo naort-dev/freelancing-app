61065a47bea4034b30e38811416dc1df5ad18f37
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-11 23:33:04 +0530

    Enhanced and refactored the users views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def display_user_action(label, path, icon_name, style, options = {})
+    link_to(path, { class: "btn btn-#{style} mt-2" }.merge(options)) do
+      sanitize("#{label} #{icon(icon_name)}")
+    end
+  end
 end
