ccba3ee1b948877f5b2fe3b1e1065578d717774d
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-11 22:44:39 +0530

    Replaced back button in the view with a helper

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module RoomsHelper
+  def display_room_action(label, path, icon_name, style, options = {})
+    link_to(path, { class: "btn btn-#{style} mr-auto" }.merge(options)) do
+      sanitize("#{label} #{icon(icon_name)}")
+    end
+  end
 end
