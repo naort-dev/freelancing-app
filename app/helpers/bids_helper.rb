5ed81360cc3bc73afe0208fdc3b9b8694ffc3077
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-12 16:39:23 +0530

    Updated the view to use chat button and submit project files button using helpers

--- app/helpers/bids_helper.rb
+++ app/helpers/bids_helper.rb
@@ -4,19 +4,27 @@ module BidsHelper
   def display_bid_document(bid, document_type)
     document = bid.send("bid_#{document_type}_document")
     if document.attached?
-      render :row, label: "#{document_type.to_s.titleize} Document",
-                   value: link_to(document.filename, url_for(document), class: 'btn btn-secondary mt-2')
+      render 'row', label: "#{document_type.to_s.titleize} Document",
+                    value: link_to(document.filename, url_for(document), class: 'btn btn-secondary mt-2')
     else
       content_tag(:p, "No #{document_type} document attached.")
     end
   end
 
   def display_bid_action(label, path, icon_name, style)
-    http_method = (:delete if icon_name == 'trash' && style == 'danger')
+    http_method = set_bid_http_method(icon_name, style)
     link_to(path,
             { class: "btn btn-#{style} mt-2", method: http_method,
               data: (http_method == :delete ? { confirm: 'Are you sure?' } : {}) }) do
       sanitize("#{label} #{icon(icon_name)}")
+    end
+  end
+
+  def set_bid_http_method(icon_name, style)
+    if icon_name == 'trash' && style == 'danger'
+      :delete
+    elsif icon_name == 'chat-dots' && style == 'info'
+      :post
     end
   end
 
