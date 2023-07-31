6a59ecae697cad7d4e11c71adab78b8b76da7304
naort-dev <naor.tzairi.upwork@gmail.com>
2023-07-31 22:30:20 +0530

    Added validation for bid_description, and the functionality to truncate bid_description in the bid_table partial

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -16,6 +16,7 @@ class Bid < ApplicationRecord
   has_one_attached :bid_design_document
   has_one_attached :bid_other_document
 
+  validates :bid_description, length: { maximum: 1024 }
   validates :bid_amount, presence: true, numericality: { greater_than: 0, less_than: 1_000_000 }
   validates :user_id, uniqueness: { scope: :project_id }
 
