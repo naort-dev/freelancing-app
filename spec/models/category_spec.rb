fe71a40d6bb045014b1c382dc1279df5b11a8ea9
naort-dev <naor.tzairi.upwork@gmail.com>
2023-08-01 15:54:02 +0530

    Wrote more rspec tests for the category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,10 +3,58 @@
 require 'rails_helper'
 
 RSpec.describe Category do
+  let(:category) { build(:category) }
+
   describe 'validations' do
-    it 'validates presence of name' do
-      category = build(:category, name: nil)
-      expect(category).not_to be_valid
+    context 'when name is present and unique' do
+      it 'is valid' do
+        expect(category).to be_valid
+      end
+    end
+
+    context 'when name is not present' do
+      it 'is not valid' do
+        category.name = nil
+        expect(category).not_to be_valid
+      end
+    end
+
+    context 'when name is not unique' do
+      it 'is not valid' do
+        create(:category, name: 'Test')
+        category.name = 'Test'
+        expect(category).not_to be_valid
+      end
+    end
+  end
+
+  describe 'associations' do
+    context 'when associated with projects and users' do
+      let!(:project) { create(:project) }
+      let!(:user) { create(:user) }
+
+      before do
+        category.projects << project
+        category.users << user
+      end
+
+      it 'has the correct projects' do
+        expect(category.projects).to include(project)
+      end
+
+      it 'has the correct users' do
+        expect(category.users).to include(user)
+      end
+    end
+
+    context 'when not associated with any projects or users' do
+      it 'has no projects' do
+        expect(category.projects).to be_empty
+      end
+
+      it 'has no users' do
+        expect(category.users).to be_empty
+      end
     end
   end
 end
